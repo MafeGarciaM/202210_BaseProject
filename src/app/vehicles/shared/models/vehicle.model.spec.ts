e1e4590b71181534f8609b5da1fe99279c9f9f24
MafeGarciaM <mf.garciam1@uniandes.edu.co>
2022-05-01 03:20:28 -0500

    feat: pruenas listar vehiculos

--- src/app/vehicles/shared/models/vehicle.model.spec.ts
+++ src/app/vehicles/shared/models/vehicle.model.spec.ts
@@ -2,6 +2,17 @@ import { Vehicle } from './vehicle.model';
 
 describe('Vehicle', () => {
   it('should create an instance', () => {
-    expect(new Vehicle()).toBeTruthy();
+    expect(
+      new Vehicle(
+        1,
+        'volkswgen',
+        'beetle',
+        'VU Express',
+        2020,
+        20000,
+        'Gris',
+        'https://cdn.group.renault.com/ren/co/vehicles/kangoo/home/renault-kangoo-exterior.jpg'
+      )
+    ).toBeTruthy();
   });
 });
--- src/app/vehicles/vehicle-list/vehicle-list.component.spec.ts
+++ src/app/vehicles/vehicle-list/vehicle-list.component.spec.ts
@@ -1,28 +1,82 @@
+import { Vehicle } from './../shared/models/vehicle.model';
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  waitForAsync,
+} from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { VehicleListComponent } from './vehicle-list.component';
 
 describe('VehicleListComponent', () => {
   let component: VehicleListComponent;
   let fixture: ComponentFixture<VehicleListComponent>;
+  let debug: DebugElement;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehicleListComponent ]
-    })
-    .compileComponents();
+      declarations: [VehicleListComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VehicleListComponent);
     component = fixture.componentInstance;
+
+    component.vehicleList = [
+      new Vehicle(
+        1,
+        'volkswgen',
+        'beetle',
+        'VU Express',
+        2020,
+        20000,
+        'Gris',
+        'https://cdn.group.renault.com/ren/co/vehicles/kangoo/home/renault-kangoo-exterior.jpg'
+      ),
+      new Vehicle(
+        1,
+        'Renault',
+        'Kangoo',
+        'VU Express',
+        2017,
+        93272,
+        'Blanco',
+        'https://cdn.group.renault.com/ren/co/vehicles/kangoo/home/renault-kangoo-exterior.jpg'
+      ),
+      new Vehicle(
+        1,
+        'Chevrolet',
+        'Aveo',
+        'Family',
+        2013,
+        19500,
+        'Gris',
+        'https://turistran.com/2-thickbox_default/chevrolet-spark-life.jpg'
+      ),
+    ];
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create a table with rows', () => {
+    let tableRows = fixture.debugElement.queryAll(By.css('tbody tr'));
+    expect(tableRows.length).toBe(3);
+  });
+
+  it('should create a table with header', () => {
+    let tableHead = fixture.debugElement.queryAll(By.css('thead'));
+    expect(tableHead.length).toBe(1);
   });
 });
